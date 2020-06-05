b5444d3645a37e1e8dbd5d9093529e209dad013e
timiv1 <timi.vovk@gmail.com>
2020-06-05 18:42:37 +0200

    proba

---
+++ Assets/ResoltManageScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Unity.Editor;
+
+
+public class ResoltManageScript : MonoBehaviour
+{
+    private Firbase
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://udariploscodb.firebaseio.com/");
+        
+    }
+}
